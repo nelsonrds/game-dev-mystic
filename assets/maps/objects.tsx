6ba915b3bb68f7e9ad7b2cf3ea7c26216e784d0a
Nelson Rodrigues <17497085+nelsonrds@users.noreply.github.com>
2025-11-17 23:48:04 +0000

    map

--- assets/maps/objects.tsx
+++ assets/maps/objects.tsx
@@ -18,6 +18,7 @@
    <property name="animation" value="IDLE"/>
    <property name="animationSpeed" type="float" value="1"/>
    <property name="atlasAsset" value="OBJECTS"/>
+   <property name="controller" type="bool" value="true"/>
    <property name="speed" type="float" value="4"/>
   </properties>
   <image source="objects/player.png" width="32" height="32"/>
