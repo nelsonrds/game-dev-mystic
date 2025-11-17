8ecebf0712b7975a785df39a552dd7b36588c357
Nelson Rodrigues <17497085+nelsonrds@users.noreply.github.com>
2025-11-17 23:47:55 +0000

    gamescreen

--- core/src/main/java/io/github/nelsonrs/mystic/screen/GameScreen.java
+++ core/src/main/java/io/github/nelsonrs/mystic/screen/GameScreen.java
@@ -6,6 +6,8 @@ import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Disposable;
 import io.github.nelsonrs.mystic.Main;
 import io.github.nelsonrs.mystic.asset.MapAsset;
@@ -15,7 +17,9 @@ import io.github.nelsonrs.mystic.system.AnimationSystem;
 import io.github.nelsonrs.mystic.system.ControllerSystem;
 import io.github.nelsonrs.mystic.system.FacingSystem;
 import io.github.nelsonrs.mystic.system.FsmSystem;
-import io.github.nelsonrs.mystic.system.MoveSystem;
+import io.github.nelsonrs.mystic.system.PhysicDebugRenderSystem;
+import io.github.nelsonrs.mystic.system.PhysicMoveSystem;
+import io.github.nelsonrs.mystic.system.PhysicSystem;
 import io.github.nelsonrs.mystic.system.RenderSystem;
 import io.github.nelsonrs.mystic.tiled.TiledAshleyConfigurator;
 import io.github.nelsonrs.mystic.tiled.TiledService;
@@ -26,21 +30,26 @@ public class GameScreen extends ScreenAdapter {
     private final TiledAshleyConfigurator tiledAshleyConfigurator;
     private final KeyboardController keyboardController;
     private final Main game;
+    private final World physicWorld;
 
     public GameScreen(Main game) {
         this.game = game;
         this.tiledService = new TiledService(game.getAssetService());
+        this.physicWorld = new World(Vector2.Zero, true);
+        this.physicWorld.setAutoClearForces(false);
         this.engine = new Engine();
-        this.tiledAshleyConfigurator = new TiledAshleyConfigurator(this.engine, game.getAssetService());
+        this.tiledAshleyConfigurator = new TiledAshleyConfigurator(this.engine, game.getAssetService(), physicWorld);
         this.keyboardController = new KeyboardController(GameControllerState.class, engine);
 
-        this.engine.addSystem(new ControllerSystem());
-        this.engine.addSystem(new MoveSystem());
-        this.engine.addSystem(new FsmSystem());
+        this.engine.addSystem(new PhysicMoveSystem());
+        this.engine.addSystem(new PhysicSystem(physicWorld, 1 / 45f));
         this.engine.addSystem(new FacingSystem());
+        this.engine.addSystem(new FsmSystem());
+        this.engine.addSystem(new ControllerSystem());
         this.engine.addSystem(new AnimationSystem(game.getAssetService()));
         // Order is important when adding systems to the engine
         this.engine.addSystem(new RenderSystem(game.getBatch(), game.getViewport(), game.getCamera()));
+        this.engine.addSystem(new PhysicDebugRenderSystem(physicWorld, game.getCamera()));
     }
 
     @Override
@@ -51,6 +60,7 @@ public class GameScreen extends ScreenAdapter {
         Consumer<TiledMap> renderConsumer = this.engine.getSystem(RenderSystem.class)::setMap;
         this.tiledService.setMapChangeConsumer(renderConsumer);
         this.tiledService.setLoadObjectConsumer(this.tiledAshleyConfigurator::onLoadObject);
+        this.tiledService.setLoadTileConsumer(tiledAshleyConfigurator::onLoadTile);
 
         TiledMap tiledMap = this.tiledService.loadMap(MapAsset.MAIN);
         this.tiledService.setMap(tiledMap);
@@ -74,5 +84,6 @@ public class GameScreen extends ScreenAdapter {
                 disposableSystem.dispose();
             }
         }
+        this.physicWorld.dispose();
     }
 }
